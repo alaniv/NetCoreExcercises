1e1be193b318d69bca939e8f809a04b63cefe87f
alaniv <ivoalani@outlook.com>
2020-02-11 18:57:37 -0300

    Chapter 01

---
+++ Chapter01/HelloCS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/*
+dotnet new console
+dotnet run
+*/
+
+namespace HelloCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!");
+        }
+    }
+}
