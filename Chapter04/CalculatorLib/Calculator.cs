ade8644f8b4d3d4d6abd96d34c4535ab31eb7cc4
alaniv <ivoalani@outlook.com>
2020-02-12 23:14:21 -0300

    Chapter04

---
+++ Chapter04/CalculatorLib/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//dotnet new classlib
+//dotnet build
+
+namespace CalculatorLib
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            //return a * b;
+            return a + b;
+        }
+    }
+}
