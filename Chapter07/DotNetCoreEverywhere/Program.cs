28e80516858a492b34e828f9771a49fc6bea172f
alaniv <ivoalani@outlook.com>
2020-02-15 17:07:20 -0300

    Chapter07

---
+++ Chapter07/DotNetCoreEverywhere/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+/*
+dotnet publish -c Release -r win10-x64
+dotnet publish -c Release -r osx-x64
+dotnet publish -c Release -r rhel.7.4-x64
+*/
+
+namespace DotNetCoreEverywhere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("I can run everywhere!");
+        }
+    }
+}
