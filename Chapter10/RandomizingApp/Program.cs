f24ef8a18c1402ce44d118575d78c61889154799
alaniv <ivoalani@outlook.com>
2020-02-19 15:21:58 -0300

    Chapter10

---
+++ Chapter10/RandomizingApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+using System.Security.Cryptography;
+namespace RandomizingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("How big do you want the key (in bytes): ");
+            string size = ReadLine();
+            byte[] key = GetRandomKeyOrIV(int.Parse(size));
+            WriteLine($"Key as byte array:");
+            for (int b = 0; b < key.Length; b++)
+            {
+                Write($"{key[b]:x2} ");
+                if (((b + 1) % 16) == 0) WriteLine();
+            }
+            WriteLine();
+        }
+
+        public static byte[] GetRandomKeyOrIV(int size)
+        {
+            var r = RandomNumberGenerator.Create();
+            var data = new byte[size];
+            r.GetNonZeroBytes(data);
+            // data is an array now filled with
+            // cryptographically strong random bytes
+            return data;
+        }
+    }
+}
